6f6ae5e9b3d451556c212761eafc95bda37245bf
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-06 20:40:55 +0200

    Tutorial checkpoint

---
+++ PL Tutorial PrimeMinisters Dictionary/PrimeMinister.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PL_Tutorial_PrimeMinisters_Dictionary
+{
+    class PrimeMinister
+    {
+        public string Name { get; private set; }
+        public int YearElected { get; private set; }
+
+        public PrimeMinister(string name, int yearElected)
+        {
+            this.Name = name;
+            this.YearElected = yearElected;
+        }
+
+        //overriding ToString() affects Console.WriteLine();
+        //because "cw" inplements ToString() to display the elements
+        public override string ToString()
+        {
+            return string.Format($"{Name}, elected: {YearElected}");
+        }
+    }
+}
--- PL Tutorial PrimeMinisters Dictionary/Program.cs
+++ PL Tutorial PrimeMinisters Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PL_Tutorial_PrimeMinisters_Dictionary
 {
@@ -6,7 +7,44 @@ namespace PL_Tutorial_PrimeMinisters_Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //dictionary with a string key
+            var primerMinisters = new Dictionary<string, PrimeMinister>
+            {
+                //proper way for creating dictionary elements (key + value)
+                {"JC", new PrimeMinister("James Callaghan", 1976) },
+                {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                {"TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+            
+
+            //assigns the value to "pm" using the "out" parameter
+            PrimeMinister pmDc;
+            bool found = primerMinisters.TryGetValue("DC", out pmDc);
+            // = if "bool found" resolved as true and value was assigned: ...
+            if (found)
+                //note we display "pm" not "found"
+                Console.WriteLine(pmDc.ToString());
+            else
+            Console.WriteLine("Value not found in Dictioary" + "\r\n");
+            //you can enumerate either the key or the value
+            foreach (var pPm in primerMinisters)
+                Console.WriteLine(pPm.Key + ",    " + pPm.Value);
+            Console.WriteLine();
+
+
+            //using TKey ro replace the elements TValue
+            primerMinisters["JC"] =
+                new PrimeMinister("Jim Callaghan", 1976);
+            //example of displaying the value of the elements only
+            foreach (var pmJc in primerMinisters.Values)
+                Console.WriteLine(pmJc);
+            Console.WriteLine();
+
+
+            //using TKey to add NewKey & NewValue
+            primerMinisters.Add("AM", new PrimeMinister("Andrei Mungiu", 2017));
+            foreach (var pAm in primerMinisters)
+                Console.WriteLine(pAm);
         }
     }
 }
