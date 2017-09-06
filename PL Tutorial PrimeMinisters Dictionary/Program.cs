52edb849bf7997c68081253cdbe179ae7a047232
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-06 17:19:14 +0200

    Add project files.

---
+++ PL Tutorial PrimeMinisters Dictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PL_Tutorial_PrimeMinisters_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
